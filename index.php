d129ffb32a7025aff4ff8e7b5142cd09b57f667d
AutWorld <jakubchmura9@gmail.com>
2023-09-11 22:16:03 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,98 @@
+
+
+<!DOCTYPE html>
+<html lang="sk" lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" >
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/png" href="pictures/sekzi boj.jpg" sizes="16x16">
+    <title>Home</title>
+    
+   
+
+
+</head>
+<body>
+
+    <!-- header -->
+    <div class="header">
+          
+        <a href="index.html" class="logo">
+            <img src="pictures/AutWorld_Logo.webp" alt="logo" class="rozmerL">
+        </a>
+        
+        <a href="index.html" class="on_page">
+            <div class="textH">HOME</div>
+        </a>
+        <a href="about_us.html">
+            <div class="textH">ABOUT US</div>
+        </a>
+        <a href="faq.html">
+            <div class="textH">FAQ</div>
+        </a>
+        
+        <a href="galery.html">
+            <div class="textH">GALERY</div>
+        </a>
+        
+    </div>
+    <div class="header1"></div>
+    <!-- body -->
+    <div class="body">
+
+        <div class="textB">HOME</div> 
+        <div class="bodys" id="bodys"></div>
+        
+        <div class="bodyFixing">
+            <div class="bodyHalf">
+                <div class="pictureL"></div>
+            </div>
+            <div class="bodyHalf">
+                <div class="pictureL" id="pictureBorder"></div>
+            </div>
+
+
+        </div>
+        
+
+    </div>
+    <div class="video">
+
+        <iframe class="vids" src="https://www.youtube.com/embed/jGxwaYyvLBI?si=lrwy4iiy1tojxIdv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+    </div>
+
+    <div class="line"></div>
+    
+    <footer class="footer">
+
+        <div class="footerL">
+
+            <div class="textF">idc1</div>
+            <div class="textF">idc2</div>
+            <div class="textF">idc3</div>
+            
+        </div>        
+
+        <div class="footerC">
+
+            <div class="textF">idc4</div>
+            <div class="textF">idc5</div>
+            <div class="textF">idc6</div>
+
+        </div>
+
+        <div class="footerR">
+
+            <div class="textF">idc7</div>
+            <div class="textF">idc8</div>
+            <div class="textF">idc9</div>
+
+        </div>
+        
+    </footer>
+    
+</body>
+</html>
